5ba88dbd97c92f078ea977ef8306ea6eb8729be2
SugerInMyPacket <51475387+SugerInMyPacket@users.noreply.github.com>
2019-06-10 10:49:31 +0800

    Update ticketState.sql

--- sqlScript/ticketState.sql
+++ sqlScript/ticketState.sql
@@ -15,3 +15,26 @@ insert into ticketState values ("ba","退票(back)");
 
 select * from ticketState;
 
+
+--插入过程
+delimiter //
+create procedure addTicketState(
+    in tktStateId char(2),
+    in tktStateDsb nvarchar(100))
+begin 
+    insert into ticketState values(tktStateId,tktStateDsb);
+end//
+delimiter ;
+
+--删除过程
+delimiter //
+create procedure removeTicketState(
+    in mTktStateId char(2),
+    in newTktStateDsb nvarchar(100))
+begin 
+    update ticketState set tktStateDsb=newTktStateDsb
+    where tktStateId=mTktStateId;
+end//
+delimiter ;
+
+
